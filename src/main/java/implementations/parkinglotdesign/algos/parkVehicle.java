09a65048af1bc50c6f5996ae87aa1e701e2df1ca
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2026-03-15 21:35:15 +0530

    added locking

--- src/main/java/implementations/parkinglotdesign/algos/parkVehicle.java
+++ src/main/java/implementations/parkinglotdesign/algos/parkVehicle.java
@@ -3,21 +3,31 @@ package implementations.parkinglotdesign.algos;
 import implementations.parkinglotdesign.*;
 import implementations.parkinglotdesign.constants.SpotType;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class parkVehicle {
 
     private SpotType type;
     private Vehicle vehicle;
     private Building building;
+    private ReentrantLock lock;
 
-    public  parkVehicle(SpotType type,Vehicle vehicle, Building building){
+    public  parkVehicle(SpotType type,Vehicle vehicle, Building building, ReentrantLock lock){
         this.type=type;
         this.vehicle=vehicle;
         this.building=building;
+        this.lock=lock;
     }
 
     public void park(){
+
         for(Floor floor :building.getFloor()){
             for(ParkingSpot spot: floor.getSpots()){
+                if(!lock.tryLock()){
+                    return;
+                }
+                
                 if(spot.isOccupied()){
                     if(vehicle.getVehicleType().equals("MOTORCYCLE") ){
                         if(spot.getSpotType().equals("SMALL")){
