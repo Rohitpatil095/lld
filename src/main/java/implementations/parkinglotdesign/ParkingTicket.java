4bab1e91f57ad1a01a451b74e96e93fbf48d580f
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2026-03-15 22:25:22 +0530

    locking mech and added parkingLot class

---
+++ src/main/java/implementations/parkinglotdesign/ParkingTicket.java
@@ -0,0 +1,17 @@
+package implementations.parkinglotdesign;
+
+public class ParkingTicket {
+    private String id;
+    private String spotID;
+    private Vehicle vehicle;
+
+    public String getSpotID() {
+        return spotID;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+
+}
