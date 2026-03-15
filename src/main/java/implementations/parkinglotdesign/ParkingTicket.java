8ddff5e0ef9d90b4b15c019bea7c307452a1dfd6
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2026-03-15 22:47:58 +0530

    updated parking lot methodes etc

--- src/main/java/implementations/parkinglotdesign/ParkingTicket.java
+++ src/main/java/implementations/parkinglotdesign/ParkingTicket.java
@@ -4,6 +4,13 @@ public class ParkingTicket {
     private String id;
     private String spotID;
     private Vehicle vehicle;
+    private int floorNumber;
+
+    public ParkingTicket(String spotID, Vehicle vehicle, int floorNumber) {
+        this.spotID = spotID;
+        this.vehicle = vehicle;
+        this.floorNumber = floorNumber;
+    }
 
     public String getSpotID() {
         return spotID;
@@ -13,5 +20,7 @@ public class ParkingTicket {
         return vehicle;
     }
 
-
+    public int getFloorNumber() {
+        return floorNumber;
+    }
 }
